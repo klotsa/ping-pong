2a9b2876f38f60fc8094dd35f90de8adb56f7727
Anatoliy Klots <klots@uw.edu>
2017-03-31 14:39:26 -0700

    add UI

--- js/scripts.js
+++ js/scripts.js
@@ -8,40 +8,36 @@ inputArray = numeralInput;
 var pingPong = ["ping", "pong", "ping-pong"];
 //number check
 if (numeralInput < 1) {
-  alert("We can't play )-:");
+  alert("Alas, you can't ping-pong this number )-:");
   } else {
 //  alert("got a number");
-} //end of number check
-
-for (var index = 1; index <= numeralInput; index++) {
-  if (index % 3 === 0 && index % 5 === 0) {
-    outputArray.push(pingPong[2]);
-    } else if (index % 5 === 0) {
-    outputArray.push(pingPong[1]);
+  for (var index = 1; index <= numeralInput; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      outputArray.push(pingPong[2]);
+      } else if (index % 5 === 0) {
+      outputArray.push(pingPong[1]);
+      }
+      else if (index % 3 === 0) {
+      outputArray.push(pingPong[0]);
+      } else {
+      outputArray.push(index)
+      }
+      //alert(outputArray);
     }
-    else if (index % 3 === 0) {
-    outputArray.push(pingPong[0]);
-    } else {
-    outputArray.push(index)
-    }
-    //alert(outputArray);
+  //alert(outputArray);
+    for (var i = 0; i < outputArray.length; i++)
+     $("#result").append(outputArray[i] + ",<br>");
   }
-//alert(outputArray);
-  for (var i = 0; i < outputArray.length; i++)
-   $("#result").append(outputArray[i] + ",<br>");
-  //results = outputArray.toString();
 };
 
 //UI
-
 $(document).ready(function() {
   $("form#numeral").submit(function(event) {
     event.preventDefault();
     var numeralInput = $("input#numeral").val();
     var result = numeral(numeralInput);
-    $("#result").text(results);
+   //  $("#result").text(results);
     $("#result").show();
     // console.log(outputFinal);
-
   });
 });
