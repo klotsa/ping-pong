e90e2f339f5ea1b3eb38215edfe967bf540b811b
Anatoliy Klots <klots@uw.edu>
2017-03-31 12:02:05 -0700

    add for loop to fill an array with elements and three if else statements to put words for every element divisible by 3 5 or 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,10 @@
 //back-end
-// var numeral = function(numeralInput) {
-// if (numeral < 1) {
-//   alert("We can't play )-:");
-//   } else {
-//   alert("Y");
-//   }
-//
-// };
+
 
 
 var numeral = function(numeralInput) {
-var numeralInput = 7; //test, replace below:
-//inputArray = numeralInput;
+//var numeralInput = 7; //test, replace below:
+inputArray = numeralInput;
 alert(inputArray);
 var inputArray = [];
 var outputArray = [];
@@ -20,16 +13,22 @@ var pingPong = ["ping", "pong", "ping-pong"];
 if (numeralInput < 1) {
   alert("We can't play )-:");
   } else {
-  alert("got a nimber");
+//  alert("got a nimber");
 } //end of number check
 
-//
-// for (var index = 1; index <= numeralInput; index += 1) {
-//  if (index % 3 === 0) {
-//  alert("ping");
-//  } else {
-//  alert(index);
-//  }
+for (var index = 1; index <= numeralInput; index++) {
+  if (index % 3 === 0 && index % 5 === 0) {
+    outputArray.push(pingPong[2]);
+    } else if (index % 5 === 0) {
+    outputArray.push(pingPong[1]);
+    }
+    else if (index % 3 === 0) {
+    outputArray.push(pingPong[0]);
+    } else {
+    outputArray.push(index)
+    }
+  }
+  alert(outputArray);
 };
 
 
@@ -40,15 +39,7 @@ $(document).ready(function() {
   $("form#numeral").submit(function(event) {
     event.preventDefault();
     var numeralInput = $("input#numeral").val();
-    // alert(numeralInput);
-//
-//
-//     if (numeralInput < 1) {
-//       alert("We can't play )-:");
-//       } else {
-//       alert("Y");
-//       }
-//
+ 
     var result = numeral(numeralInput);
 //     // $("#result").text(outputFinal);
     $("#result").show();
